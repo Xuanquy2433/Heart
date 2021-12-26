b17c1c920028693c221c050d1e7006b754e2a8ee
XuanQuy <nhokxuanquy@gmail.com>
2021-12-26 08:42:54 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,16 @@
+var audio = new Audio('nga.mp3');
+
+function music() {
+
+    
+    console.log("yeu cau");
+    audio.play();
+    
+}
+function stopMusic() {
+
+   
+    console.log("tat");
+    audio.pause();
+    
+}
